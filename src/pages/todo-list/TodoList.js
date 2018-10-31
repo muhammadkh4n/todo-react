16114c1969e0ffdabbdbc52496a847031b68f276
Muhammad Khan <muhammadkh4n@gmail.com>
2018-10-31 18:01:13 +0500

    todo list page

---
+++ src/pages/todo-list/TodoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './TodoList.css';
+
+class TodoList extends Component {
+  render() {
+    return (
+      <div id="todo-list-page">
+        list
+      </div>
+    );
+  }
+}
+
+export default TodoList;
